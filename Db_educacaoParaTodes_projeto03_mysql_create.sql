8e07858da8f1bdae5f8098ec864a3ffd21df11dc
LuLenny <lucas.sabido@gmail.com>
2021-06-03 11:29:17 -0300

    Atualização do DER corrigida

--- Db_educacaoParaTodes_projeto03_mysql_create.sql
+++ Db_educacaoParaTodes_projeto03_mysql_create.sql
@@ -18,7 +18,6 @@ CREATE TABLE `usuario` (
 	`nome` varchar(255) NOT NULL,
 	`email` varchar(255) NOT NULL,
 	`senha` varchar(255) NOT NULL,
-	`senha` varchar(255) NOT NULL,
 	`voluntario` BOOLEAN NOT NULL,
 	PRIMARY KEY (`id`)
 );
